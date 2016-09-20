b83b16d14d124665919c2e969599c2fadd40bc5a
kaylynyuh <yuhkay4@gmail.com>
2016-09-20 15:30:20 -0700

    reading from json successfully, POST working

--- server.js
+++ server.js
@@ -14,25 +14,34 @@ const server = http.createServer(function(req, res) {
   req.url = url.parse(req.url);
   req.url.query = queryString.parse(req.url.query);
 
-  if(req.method === 'POST') {
-    parseBody(req, function(err) {
-      if (err) return console.error(err);
-    });
-  }
-
   if(req.method === 'GET' && req.url.pathname === '/') {
-    res.writeHead(200,'{Content-Type: text/plain}');
+    res.writeHead(200,{'Content-Type': 'text/plain'});
   }
 
   if(req.method === 'GET' && req.url.pathname === '/cowsay' && req.url.query.text) {
     res.writeHead(200);
     res.write(cowsay.say({text: req.url.query.text}));
     res.end();
+  } else if (!req.url.query.text) {
+    res.writeHead(400);
+    res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}));
+    res.end();
   }
 
-  res.statuscode = 404;
-  res.write('no such luck');
-  res.end();
+  if(req.method === 'POST' && req.url.pathname === '/cowsay') {
+    parseBody(req, function(err) {
+      if (err) return console.error(err);
+      if(req.body.text){
+        res.writeHead(200,{'Content-Type': 'text/plain'});
+        res.write(cowsay.say({text: req.body.text}));
+        res.end();
+      } else {
+        res.writeHead(400);
+        res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}));
+        res.end();
+      }
+    });
+  }
 });
 
 server.listen(PORT, function() {
